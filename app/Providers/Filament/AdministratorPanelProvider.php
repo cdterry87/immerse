ea67c57ebe546c55a2e9de7fc9fc5e1da33aa4fe
Chase Terry <cterry@lightbeacon.com>
2025-11-10 16:56:14 -0600

    fixes admin panel resources

--- app/Providers/Filament/AdministratorPanelProvider.php
+++ app/Providers/Filament/AdministratorPanelProvider.php
@@ -35,12 +35,12 @@ class AdministratorPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Cyan,
             ])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->discoverResources(in: app_path('Filament/Administrator/Resources'), for: 'App\\Filament\\Administrator\\Resources')
+            ->discoverPages(in: app_path('Filament/Administrator/Pages'), for: 'App\\Filament\\Administrator\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Administrator/Widgets'), for: 'App\\Filament\\Administrator\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
